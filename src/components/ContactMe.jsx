cea4ba650a6a3696de59a0a3b497299767687363
ViperXD99 <sandaruxd99@gmail.com>
2023-09-12 15:23:59 +0530

    Update ContactMe.jsx

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -12,7 +12,7 @@ const ContactMe = () => {
     window.location.href = contactOptions.linkedIn;
   };
   return (
-    <div className="flex flex-col justify-top items-center max-container py-6  h-auto scroll-smooth ">
+    <div className="flex flex-col justify-top items-center text-center max-container py-6 px-4 h-auto scroll-smooth ">
       <p className="text-18px-48px animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-black">
         Get in Touch
       </p>
@@ -21,18 +21,23 @@ const ContactMe = () => {
         Got a project you would like me to work on? Or how about just a friendly
         chat?
       </p>
-      <div className="flex flex-row gap-10 items-center mt-6">
-        <button
-          className="border border-slate-50 border-x-4 border-y-4 rounded-full w-[3rem] h-[3rem] p-1"
-          onClick={contactViaEmail}
-        >
-          <img
-            src={mail}
-            alt="Linkedin Icon"
-            className="w-full h-full object-cover"
-          />
-        </button>
-        <p className=" cursor-pointer" onClick={contactViaEmail} >sandaruxd99@gmail.com</p>
+      <div className="flex flex-col md:flex-row gap-10 items-start mt-6">
+        <div className="flex flex-row items-center  space-x-3">
+          <button
+            className="border border-slate-50 border-x-4 border-y-4 rounded-full w-[3rem] h-[3rem] p-1"
+            onClick={contactViaEmail}
+          >
+            <img
+              src={mail}
+              alt="Linkedin Icon"
+              className="w-full h-full object-cover"
+            />
+          </button>
+          <p className=" cursor-pointer" onClick={contactViaEmail}>
+            sandaruxd99@gmail.com
+          </p>
+        </div>
+        <div className="flex flex-row items-center space-x-3">
         <button
           className="rounded-full w-[3rem] h-[3rem] p-1 bg-white"
           onClick={contactViaLinkedIn}
@@ -43,15 +48,19 @@ const ContactMe = () => {
             className="w-full h-full object-cover"
           />
         </button>
-        <p className=" cursor-pointer" onClick={contactViaEmail} >Sandaru Perera</p>
+        <p className=" cursor-pointer" onClick={contactViaEmail}>
+          Sandaru Perera
+        </p>
+        </div>
+        
       </div>
-      <p className="flex flex-row text-sm font-thin mt-28" >
+      <p className="flex flex-row text-[12px] font-thin mt-28">
         Copyright
         <span>
           <img
             src={copyright}
             alt="Copyright Icon"
-            className="w-[20px] h-[20px]] object-cover"
+            className="w-[14px] h-[14px]] object-cover"
           />
         </span>
         2023 Sandaru Perera, All Rights Reserved
