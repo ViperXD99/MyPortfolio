b017169973a8a84d114c614ed1785915eeb7befa
ViperXD99 <sandaruxd99@gmail.com>
2023-09-13 09:46:04 +0530

    Update TypeWriterEffect.jsx

--- src/components/TypeWriterEffect.jsx
+++ src/components/TypeWriterEffect.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import TypeWriter from "typewriter-effect";
 const TypeWriterEffect = () => {
 
-    const strings = ['UI/UX Designer', 'Full Stack Developer']
+    const strings = ['Full Stack Developer']
   return (
     <div className=' text-fluid-2 animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-black whitespace-nowrap'>
         <TypeWriter
